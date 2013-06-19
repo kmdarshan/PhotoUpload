16ba4aa7e0c591d6cca2c1c62b04bf1bf3d5989b
darshan <kmdarshan@gmail.com>
2013-06-18 17:59:11 -0700

    Adding first files for photo upload q's

--- PhotoUpload/RFAppDelegate.m
+++ PhotoUpload/RFAppDelegate.m
@@ -16,7 +16,7 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.viewController = [[RFViewController alloc] initWithNibName:@"RFViewController" bundle:nil];
+    self.viewController = [[RFViewController alloc] init];
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
     return YES;
--- PhotoUpload/RFViewController.m
+++ PhotoUpload/RFViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "RFViewController.h"
+#import "AFHTTPClient.h"
+#import "AFHTTPRequestOperation.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface RFViewController ()
 
@@ -17,7 +20,29 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self.view setFrame:CGRectMake(0, 0, 320, 680)];
+    [self.view setBackgroundColor:[UIColor colorWithRed:208.0/255.0 green:204.0/255.0 blue:219.0/255.0 alpha:1.0]];
+    [self uploadPhotoToServer:@""];
+}
+
+-(void) uploadPhotoToServer:(NSString*) path {
+    
+    AFHTTPClient *httpClient = [AFHTTPClient clientWithBaseURL:[NSURL URLWithString:@"http://172.22.4.203:8888"]];
+    NSMutableURLRequest *afRequest = [httpClient multipartFormRequestWithMethod:@"POST" path:@"/videograms/upload.php" parameters:nil constructingBodyWithBlock:^(id <AFMultipartFormData>formData)
+                                      {
+                                        UIImage *image = [UIImage imageNamed:@"upload.png"];
+                                        NSData *photoData = UIImageJPEGRepresentation(image, 1.0);
+                                        [formData appendPartWithFileData:photoData name:@"image" fileName:path mimeType:@"image/jpeg"];
+                                      }];
+    
+    
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:afRequest];
+    [operation  setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"Success");
+    }failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"error: %@ %@ ",  operation.responseString, error);
+    }];
+    [operation start];
 }
 
 - (void)didReceiveMemoryWarning
