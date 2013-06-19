59295a6aaa7c0587d5f2cfc3a8685d492111ac0b
darshan <kmdarshan@gmail.com>
2013-06-18 18:35:17 -0700

    adding percent indicator

--- PhotoUpload/RFViewController.m
+++ PhotoUpload/RFViewController.m
@@ -11,8 +11,9 @@
 #import "AFHTTPRequestOperation.h"
 #import <QuartzCore/QuartzCore.h>
 
-@interface RFViewController ()
-
+@interface RFViewController (){
+    UIView *blockview, *progressBar;
+}
 @end
 
 @implementation RFViewController
@@ -22,7 +23,24 @@
     [super viewDidLoad];
     [self.view setFrame:CGRectMake(0, 0, 320, 680)];
     [self.view setBackgroundColor:[UIColor colorWithRed:208.0/255.0 green:204.0/255.0 blue:219.0/255.0 alpha:1.0]];
-    [self uploadPhotoToServer:@""];
+    [self uploadPhotoToServer:@"upload.png"];
+    
+    [self setupProgressbar];
+}
+
+-(void) setupProgressbar {
+    
+    blockview = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width - 10, 20)];
+    [self.view addSubview:blockview];
+    [blockview setBackgroundColor:[UIColor clearColor]];
+    [[blockview layer] setBorderColor:[UIColor blackColor].CGColor];
+    [[blockview layer] setBorderWidth:3.0f];
+    [blockview setCenter:self.view.center];
+    
+    progressBar = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 1, 18)];
+    [progressBar setBackgroundColor:[UIColor redColor]];
+    [blockview addSubview:progressBar];
+    
 }
 
 -(void) uploadPhotoToServer:(NSString*) path {
@@ -30,19 +48,23 @@
     AFHTTPClient *httpClient = [AFHTTPClient clientWithBaseURL:[NSURL URLWithString:@"http://172.22.4.203:8888"]];
     NSMutableURLRequest *afRequest = [httpClient multipartFormRequestWithMethod:@"POST" path:@"/videograms/upload.php" parameters:nil constructingBodyWithBlock:^(id <AFMultipartFormData>formData)
                                       {
-                                        UIImage *image = [UIImage imageNamed:@"upload.png"];
+                                        UIImage *image = [UIImage imageNamed:path];
                                         NSData *photoData = UIImageJPEGRepresentation(image, 1.0);
-                                        [formData appendPartWithFileData:photoData name:@"image" fileName:path mimeType:@"image/jpeg"];
+                                        [formData appendPartWithFileData:photoData name:@"file" fileName:path mimeType:@"image/jpeg"];
                                       }];
     
-    
-    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:afRequest];
-    [operation  setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
-        NSLog(@"Success");
-    }failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-        NSLog(@"error: %@ %@ ",  operation.responseString, error);
+    __block int countr = 10;
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:afRequest];    
+    [operation setUploadProgressBlock:^(NSUInteger bytesWritten, long long totalBytesWritten, long long totalBytesExpectedToWrite) {
+        double val = (totalBytesWritten / totalBytesExpectedToWrite) * 100;
+        NSLog(@"Sent %lld of %lld bytes percent %lld", totalBytesWritten, totalBytesExpectedToWrite, (totalBytesWritten / totalBytesExpectedToWrite) * 100);
+        [UIView animateWithDuration:0.15f animations:^{
+            
+            [progressBar setFrame:CGRectMake(0, 0, countr, 18)];
+            
+        }];
     }];
-    [operation start];
+    [httpClient enqueueHTTPRequestOperation:operation];
 }
 
 - (void)didReceiveMemoryWarning
