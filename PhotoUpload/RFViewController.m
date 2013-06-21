d9e258b2b8800abee37855c8f21d60c330ad6984
darshan <kmdarshan@gmail.com>
2013-06-20 17:54:30 -0700

    commit

--- PhotoUpload/RFViewController.m
+++ PhotoUpload/RFViewController.m
@@ -23,45 +23,55 @@
     [super viewDidLoad];
     [self.view setFrame:CGRectMake(0, 0, 320, 680)];
     [self.view setBackgroundColor:[UIColor colorWithRed:208.0/255.0 green:204.0/255.0 blue:219.0/255.0 alpha:1.0]];
+
     [self uploadPhotoToServer:@"upload.png"];
-    
     [self setupProgressbar];
+    
 }
 
 -(void) setupProgressbar {
     
-    blockview = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width - 10, 20)];
+    blockview = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width - 100, 10)];
     [self.view addSubview:blockview];
-    [blockview setBackgroundColor:[UIColor clearColor]];
+    [blockview setBackgroundColor:[UIColor lightGrayColor]];
     [[blockview layer] setBorderColor:[UIColor blackColor].CGColor];
-    [[blockview layer] setBorderWidth:3.0f];
+    [[blockview layer] setCornerRadius:5.0f];
+    [[blockview layer] setBorderWidth:1.0f];
     [blockview setCenter:self.view.center];
     
-    progressBar = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 1, 18)];
-    [progressBar setBackgroundColor:[UIColor redColor]];
+    progressBar = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 1, 9)];
+    [[progressBar layer] setCornerRadius:5.0f];
+    [progressBar setBackgroundColor:[UIColor whiteColor]];
     [blockview addSubview:progressBar];
     
 }
 
 -(void) uploadPhotoToServer:(NSString*) path {
     
-    AFHTTPClient *httpClient = [AFHTTPClient clientWithBaseURL:[NSURL URLWithString:@"http://172.22.4.203:8888"]];
+    AFHTTPClient *httpClient = [AFHTTPClient clientWithBaseURL:[NSURL URLWithString:@"http://www.kmdarshan.com"]];
     NSMutableURLRequest *afRequest = [httpClient multipartFormRequestWithMethod:@"POST" path:@"/videograms/upload.php" parameters:nil constructingBodyWithBlock:^(id <AFMultipartFormData>formData)
                                       {
                                         UIImage *image = [UIImage imageNamed:path];
                                         NSData *photoData = UIImageJPEGRepresentation(image, 1.0);
                                         [formData appendPartWithFileData:photoData name:@"file" fileName:path mimeType:@"image/jpeg"];
                                       }];
-    
-    __block int countr = 10;
-    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:afRequest];    
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:afRequest];
     [operation setUploadProgressBlock:^(NSUInteger bytesWritten, long long totalBytesWritten, long long totalBytesExpectedToWrite) {
-        double val = (totalBytesWritten / totalBytesExpectedToWrite) * 100;
-        NSLog(@"Sent %lld of %lld bytes percent %lld", totalBytesWritten, totalBytesExpectedToWrite, (totalBytesWritten / totalBytesExpectedToWrite) * 100);
-        [UIView animateWithDuration:0.15f animations:^{
-            
-            [progressBar setFrame:CGRectMake(0, 0, countr, 18)];
-            
+
+        NSDecimalNumber *hundredNumber = [[NSDecimalNumber alloc] initWithInt:100];
+        NSDecimalNumber *dnumber = [[NSDecimalNumber alloc] initWithUnsignedLong:totalBytesExpectedToWrite];
+        NSDecimalNumber *vnumber = [[NSDecimalNumber alloc] initWithUnsignedLong:totalBytesWritten];
+        NSDecimalNumberHandler *roundUp = [NSDecimalNumberHandler
+                                           decimalNumberHandlerWithRoundingMode:NSRoundUp
+                                           scale:0
+                                           raiseOnExactness:NO
+                                           raiseOnOverflow:NO
+                                           raiseOnUnderflow:NO
+                                           raiseOnDivideByZero:YES];
+        NSDecimalNumber *finalnumber = [[vnumber decimalNumberByDividingBy:dnumber] decimalNumberByMultiplyingBy:hundredNumber withBehavior:roundUp];
+        [UIView animateWithDuration:1.0f animations:^{
+            int percentProgressBar = blockview.frame.size.width * [finalnumber intValue] / 100;
+            [progressBar setFrame:CGRectMake(0, 0, percentProgressBar, progressBar.frame.size.height)];
         }];
     }];
     [httpClient enqueueHTTPRequestOperation:operation];
